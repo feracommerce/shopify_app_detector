a2ac27990d4471b3220119c0e71696d5b248091b
Jay El-Kaake <najibkaake@gmail.com>
2018-12-14 13:10:31 -0500

    Update app description for fera.ai

--- data/shopify_apps.js
+++ data/shopify_apps.js
@@ -2,7 +2,7 @@
 var SHOPIFY_APPS = [
     {
         name: 'Fera.ai', // Keep this under 20 characters
-        short_description: 'Real-time personalization (timers, social proof, counters, etc.)',  // Keep this under 75 characters
+        short_description: 'Real-time personalization + CRO (w/ timers, social proof, counters, etc.)',  // Keep this under 75 characters
         long_description: 'Real-time personalization and conversion rate optimization platform with built-in promo bars, popups, timers, social proof, counters, etc.', 
         app_store_url: 'https://apps.shopify.com/fera', 
         website_url: "https://www.fera.ai",
